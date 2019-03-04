134d4f5105fcef7cdcf25b1cd369ec765f61cf43
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-04 21:39:06 +0100

    added server listen and get

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //import package mysql
 const mysql = require("mysql");
+const express = require("express");
+var app = express();
+const bodyparser = require("body-parser");
+
+app.use(bodyparser.json());
 
 //create mysql connection
 var mysqlConnection = mysql.createConnection({
@@ -11,5 +16,19 @@ var mysqlConnection = mysql.createConnection({
 
 mysqlConnection.connect(err => {
   if (!err) console.log("DB connection succeded.");
-  else console.log("DB connection failed \n Error : " + JSON.stringify(err, undefined, 2));
+  else
+    console.log(
+      "DB connection failed \n Error : " + JSON.stringify(err, undefined, 2)
+    );
+});
+
+app.listen(3000, () =>
+  console.log("Express server is running at port number 3000")
+);
+
+app.get("/employees", (req, res) => {
+  mysqlConnection.query("SELECT * FROM employee", (err, rows, fields) => {
+    if (!err) console.log(rows[0].EmpID);
+    else console.log(err);
+  });
 });
