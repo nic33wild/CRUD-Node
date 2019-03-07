e42e57a6667d94d6c7d6ff416fa885d43b43ece5
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-07 17:20:30 +0100

    Added get delete insert update

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ var mysqlConnection = mysql.createConnection({
   host: "localhost",
   user: "nicola",
   password: "casa",
-  database: "employenodedb"
+  database: "employenodedb",
+  multipleStatements: true
 });
 
 mysqlConnection.connect(err => {
@@ -26,9 +27,70 @@ app.listen(3000, () =>
   console.log("Express server is running at port number 3000")
 );
 
+//Get all employees
 app.get("/employees", (req, res) => {
   mysqlConnection.query("SELECT * FROM employee", (err, rows, fields) => {
-    if (!err) console.log(rows[0].EmpID);
+    if (!err) res.send(rows);
     else console.log(err);
   });
+});
+
+//Get an employee
+app.get("/employees/:id", (req, res) => {
+  mysqlConnection.query(
+    "SELECT * FROM employee WHERE EmpID = ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+});
+
+//Delete an employee
+app.delete("/employees/:id", (req, res) => {
+  mysqlConnection.query(
+    "DELETE FROM employee WHERE EmpID = ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (!err) res.send("Deleted successfully.");
+      else console.log(err);
+    }
+  );
+});
+
+//Insert an employee
+app.post("/employees/", (req, res) => {
+  let emp = req.body;
+  var sql =
+    "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+  CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
+  mysqlConnection.query(
+    sql,
+    [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+    (err, rows, fields) => {
+      if (!err)
+        rows.forEach(element => {
+          if (element.constructor == Array)
+            res.send("Inserted employee id : " + element[0].EmpID);
+        });
+      else console.log(err);
+    }
+  );
+});
+
+//UPDATE an employee
+app.put("/employees/", (req, res) => {
+  let emp = req.body;
+  var sql =
+    "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+  CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
+  mysqlConnection.query(
+    sql,
+    [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+    (err, rows, fields) => {
+      if (!err) res.send("Updated succesfully!");
+      else console.log(err);
+    }
+  );
 });
